ea7e51c45c61e9da5322464ea6db9077dee18c30
jarvis394 <tarnatovski@gmail.com>
2024-04-04 04:21:16 +0300

    Fix lint-staged script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,15 @@
 module.exports = {
-  'packages/**/*.{ts,tsx}': (files) => {
+  'apps/**/*.{ts,tsx}': (files) => {
     return `nx affected --target=typecheck --files=${files.join(',')}`
   },
-  'packages/**/*.{js,ts,jsx,tsx,json}': [
+  'apps/**/*.{js,ts,jsx,tsx,json}': [
+    (files) => `nx affected:lint --files=${files.join(',')}`,
+    (files) => `nx format:write --files=${files.join(',')}`,
+  ],
+  'libs/**/*.{ts,tsx}': (files) => {
+    return `nx affected --target=typecheck --files=${files.join(',')}`
+  },
+  'libs/**/*.{js,ts,jsx,tsx,json}': [
     (files) => `nx affected:lint --files=${files.join(',')}`,
     (files) => `nx format:write --files=${files.join(',')}`,
   ],
